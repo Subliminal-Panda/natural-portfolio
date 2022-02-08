9b7ad9f600307afaa6f4789b39d414dfd14bb1aa
Tristan Alexander Mouritsen <tmouritsen57@gmail.com>
2022-02-08 10:51:52 -0700

    updated node modules, removed unnecessary dependencies

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-﻿var gulp = require('gulp');
-var gzip = require('gulp-gzip');
-
-gulp.task('compress', function() {
-  return gulp.src(['./dist/**/*.*'])
-      .pipe(gzip())
-      .pipe(gulp.dest('./dist'));
-});
