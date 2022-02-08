dfc1aeefe6b9ed09f28215f20b9b9b9a79a08e29
Tristan Alexander Mouritsen <tmouritsen57@gmail.com>
2022-02-07 17:27:42 -0700

    initial commit, created app and a few components and basic styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    NavbarComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
