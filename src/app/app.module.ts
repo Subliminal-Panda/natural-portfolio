af09780921b6f66d6ee81f4b60370121389f1880
Tristan Alexander Mouritsen <tmouritsen57@gmail.com>
2022-02-08 14:14:36 -0700

    relocated components for organized file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LogoComponent } from './logo/logo.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { PublicationComponent } from './components/publication/publication.component';
+import { AddPublicationComponent } from './components/add-publication/add-publication.component';
+import { PublicationsListComponent } from './components/publications-list/publications-list.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +30,28 @@ import { LogoComponent } from './logo/logo.component';
     FooterComponent,
     BodyComponent,
     NavbarComponent,
-    LogoComponent
+    LogoComponent,
+    PublicationComponent,
+    AddPublicationComponent,
+    PublicationsListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBsO-E829V6dg-zpLaIMVh9vqW8ROTK8ro",
+    authDomain: "nature-portfolio-3b346.firebaseapp.com",
+    databaseURL: "https://nature-portfolio-3b346-default-rtdb.firebaseio.com",
+    projectId: "nature-portfolio-3b346",
+    storageBucket: "nature-portfolio-3b346.appspot.com",
+    messagingSenderId: "147305952973",
+    appId: "1:147305952973:web:6d68373cc46cc2f9059b84",
+    measurementId: "G-4232TZN14H"
+  },
   production: false
 };
 
