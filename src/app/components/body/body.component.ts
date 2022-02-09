a12f2c1c8c7d32e26fa3a5fa3d9e74e3bee395d6
Tristan Alexander Mouritsen <tmouritsen57@gmail.com>
2022-02-09 11:55:10 -0700

    fixed styles to remove horizontal scroll behavior

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.scss']
 })
+
 export class BodyComponent implements OnInit {
 
   constructor() { }
